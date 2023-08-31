f88f2bb9872076a082ad28a374259f4a37fbaa9f
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-08-31 12:49:07 -0500

    chore: Add binary target to the package

--- Package.swift
+++ Package.swift
@@ -1,41 +1,29 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let packageVersion = "0.0.3"
+
 let package = Package(
-    name: "TruVideoCamera",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v16),
-    ],
+    name: "TruvideoSdkCamera",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TruVideoCamera",
-            targets: ["TruVideoCamera"]),
+            name: "TruvideoSdkCamera",
+            targets: ["TruvideoSdkCamera"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.6.1"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
-        .package(url: "https://github.com/Truvideo/truvideo-sdk-ios-common.git", from: "0.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TruVideoCamera",
-            dependencies: [
-                "Alamofire",
-                .product(name: "Common", package: "truvideo-sdk-ios-common"),
-                "KeychainAccess"
-            ],
-            path: "Sources"
+        .binaryTarget(
+            name: "TruvideoSdkCamera",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/\(packageVersion)/TruvideoSdkCamera.xcframework.zip",
+            checksum: "9af5cf976ac115bfb4326501045fdfdcbcfe4d259dd21028b77009f41d2d3214"
         ),
-        .testTarget(
-            name: "TruVideoCameraTests",
-            dependencies: ["TruVideoCamera"]),
     ]
 )
