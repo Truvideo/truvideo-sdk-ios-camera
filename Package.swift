cb25909f4c146f9527e44d6610781182dea00234
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-08-31 15:52:16 -0500

    chore: Bump to 0.0.5

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@
 
 import PackageDescription
 
-let packageVersion = "0.0.4"
+let commonPackageVersion = "0.0.3"
+let packageVersion = "0.0.5"
 
 let package = Package(
     name: "TruvideoSdkCamera",
@@ -21,9 +22,22 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
+            name: "Common",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(commonPackageVersion)/Common.xcframework.zip",
+            checksum: "52c0e808ce21419edb3f0b6f2c9551821f8995caa975db0b901c6afad176610f"
+        ),
+        .binaryTarget(
             name: "TruvideoSdkCamera",
             url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/\(packageVersion)/TruvideoSdkCamera.xcframework.zip",
             checksum: "a650414b97919c52bcb78b2683860db2ed3bd6b957f60cf828ecf7d7c57e338f"
         ),
+        .target(
+            name: "TruvideoSdkCameraTargets",
+            dependencies: [
+                .target(name: "Common"),
+                .target(name: "TruvideoSdkCamera")
+            ],
+            path: "Sources"
+        )
     ]
 )
