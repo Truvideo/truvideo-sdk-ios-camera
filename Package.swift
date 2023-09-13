c3b6729ae5370f6b747e68419b416e671147b91e
Jorge Orjuela <111378570+jorge-jcrew@users.noreply.github.com>
2023-09-13 10:13:47 -0500

    chore: Bump to 0.0.7

--- Package.swift
+++ Package.swift
@@ -3,8 +3,9 @@
 
 import PackageDescription
 
-let commonPackageVersion = "0.0.4"
-let packageVersion = "0.0.6"
+let commonPackageVersion = "0.0.1"
+let packageVersion = "0.0.7"
+let sharedPackageVersion = "0.0.3"
 
 let package = Package(
     name: "TruvideoSdkCamera",
@@ -12,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TruvideoSdkCamera",
-            targets: ["TruvideoSdkCamera"]),
+            targets: ["TruvideoSdkCameraTargets"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,18 +24,22 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "Common",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(commonPackageVersion)/Common.xcframework.zip",
-            checksum: "4e029bf878f4d9a6eecd9a9ac695de840b204f0bba0181681fc583161e69a362"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common-wrapper/releases/download/\(commonPackageVersion)/Common.xcframework.zip",
+            checksum: "0d4b5f63a41b265a9522f8957785bdb04c355a325cb38a9bff760281e96d7d38"
+        ),
+        .binaryTarget(
+            name: "Shared",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-common/releases/download/\(sharedPackageVersion)/shared.xcframework.zip",
+            checksum: "5028e25b25b24e6f35fbe4484dfc438a31345a9d5bf5666640e99849cfb497cb"
         ),
         .binaryTarget(
             name: "TruvideoSdkCamera",
             url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/\(packageVersion)/TruvideoSdkCamera.xcframework.zip",
-            checksum: "a650414b97919c52bcb78b2683860db2ed3bd6b957f60cf828ecf7d7c57e338f"
+            checksum: "3d76fff01b86f4d1d660752e29236f585d2534443a0e5321b96ddfbea94a3d97"
         ),
         .target(
             name: "TruvideoSdkCameraTargets",
             dependencies: [
-                .target(name: "Common"),
                 .target(name: "TruvideoSdkCamera")
             ],
             path: "Sources"
