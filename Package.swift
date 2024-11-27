e9970640ead99d73c96735887190fc04966bdc9a
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-11-27 20:35:09 +0000

    Update 76.3.1-BETA.67

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkCamera",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/76.2.1/TruvideoSdkCamera.xcframework.zip",
-            checksum: "bf568e12cd18b6424cc1fe658ac7be6b976a8cf44d51118dfc8095ed1043e210"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/76.3.1-BETA.67/TruvideoSdkCamera.xcframework.zip",
+            checksum: "bd55b3583f9f640e783c18261207a812a35d8e352866ec8516a247066eae10e3"
         ),
         .target(
             name: "TruvideoSdkCameraTargets",
