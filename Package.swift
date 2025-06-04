55c559b4d6bd2be4288325796567ce7e70ad550a
Bitrise CI <noreply@bitrise.io>
2025-06-04 15:10:19 +0000

    Update 76.7.1-RC.201

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkCamera",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/76.6.1/TruvideoSdkCamera.xcframework.zip",
-            checksum: "5fc6effc1e69d9e8b5d227852c117efaca003951a2447ad011b6cccca3248471"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/76.7.1-RC.201/TruvideoSdkCamera.xcframework.zip",
+            checksum: "131b84668b79ba83de5e980f39b8e927371ed98548b94d2019a2d4a57a715cde"
         ),
         .target(
             name: "TruvideoSdkCameraTargets",
