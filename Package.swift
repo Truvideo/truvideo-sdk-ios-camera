39cda3b4b1c1b81985396a64c27648c83176143c
jorge-pink-tech <jorge@pink-tech.io>
2025-09-09 02:34:34 -0500

    fix url version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkCamera",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/78.1.1-BETA.315/TruvideoSdkCamera.xcframework.zip",
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/78.1.1-BETA.316/TruvideoSdkCamera.xcframework.zip",
             checksum: "83aa90f28a146ba8bbc2b00441d2e6e45559c65f552bcd95cdde1b13cb0c2323"
         ),
         .target(
