47afba9cb75513ba728c2ceedd03498941e9d427
jorge-orjuela-kenility <jorge.orjuela@kenility.com>
2025-12-05 13:47:47 -0500

    Update 78.2.2-RC.5

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TruvideoSdkCamera",
-            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/78.2.2-BETA.4/TruvideoSdkCamera.xcframework.zip",
-            checksum: "d241a58e77d5d61f4cbba2db1fcb48c2305a8e07c63656da89032ae2d806263a"
+            url: "https://github.com/Truvideo/truvideo-sdk-ios-camera/releases/download/78.2.2-RC.5/TruvideoSdkCamera.xcframework.zip",
+            checksum: "023f2c2d926e9c22318f70490f3f2ccf5a65919ed21a7c365c8f5591721d6f0e"
         ),
         .target(
             name: "TruvideoSdkCameraTargets",
